1e562f1652abb391f50750823c510f12929486eb
dexterchan <dexterchan@gmail.com>
2019-12-20 21:42:19 +0800

    first commit

---
+++ vidly/src/components/common/input.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Input = props => {
+  const { label, name, type, onChange, value, error } = props;
+
+  return (
+    <div className="form-group">
+      <label htmlFor={name}>{label}</label>
+      <input
+        autoFocus
+        id={name}
+        value={value}
+        type={type}
+        className="form-control"
+        onChange={e => onChange(e)}
+      />
+      {error && <div className="alert alert-danger">{error}</div>}
+    </div>
+  );
+};
+
+export default Input;
---
+++ vidly/src/components/loginForm.jsx
@@ -0,0 +1,92 @@
+import React, { Component } from "react";
+import Joi from "joi-browser";
+import Input from "./common/input";
+class LoginForm extends Component {
+  //username = React.createRef();
+  state = {
+    account: {
+      username: "",
+      password: ""
+    },
+    errors: {}
+  };
+  componentDidMount() {
+    // this.username.current.focus();
+  }
+  schema = {
+    username: Joi.string()
+      .required()
+      .label("User Name"),
+    password: Joi.string()
+      .required()
+      .label("Password")
+  };
+  validate() {
+    const { account } = this.state;
+    const { error } = Joi.validate(account, this.schema, { abortEarly: false });
+    if (!error) return null;
+
+    const errors = {};
+    for (let item of error.details) errors[item.path[0]] = item.message;
+    return errors;
+  }
+  validateProperty({ id: name, value }) {
+    const obj = { [name]: value };
+    const schema = { [name]: this.schema[name] };
+    const { error } = Joi.validate(obj, schema, { abortEarly: false });
+    return error ? error.details[0].message : null;
+  }
+  handleSubmit = e => {
+    e.preventDefault(); // avoid full page reload
+    //Call the server
+    //Not encouraged to access real DOM like below
+    //const username = document.getElementById("username").value;
+    //const username = this.username.current.value;
+    const errors = this.validate();
+    this.setState({ errors: errors || {} });
+    if (errors) return;
+
+    //console.log(`Submitted:'${username}'`);
+  };
+  handleChange = ({ currentTarget: input }) => {
+    const errors = { ...this.state.errors };
+    const errorMessage = this.validateProperty(input);
+    if (errorMessage) {
+      errors[input.id] = errorMessage;
+    } else delete errors[input.name];
+    const account = { ...this.state.account };
+    account[input.id] = input.value;
+    this.setState({ account, errors });
+  };
+  render() {
+    const { username, password } = this.state.account;
+    const { errors } = this.state;
+    return (
+      <div>
+        <h1>Login</h1>
+        <form onSubmit={this.handleSubmit}>
+          <Input
+            label={"UserName"}
+            name={"username"}
+            type={"text"}
+            onChange={this.handleChange}
+            value={username}
+            error={errors.username}
+          />
+          <Input
+            label={"Password"}
+            name={"password"}
+            type={"password"}
+            onChange={this.handleChange}
+            value={password}
+            error={errors.password}
+          />
+
+          <button className="btn btn-primary">Login</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default LoginForm;
